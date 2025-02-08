3e03c24baed9041a98ed1a6c73d20e1ab7b8b08c
Austin Schaefer <schaefer.austin.p@inbox.eu>
2025-02-08 20:39:56 +0100

    Add formatter, use Palantir format.

--- src/test/java/com/aps/shop/DoorholderApplicationTests.java
+++ src/test/java/com/aps/shop/DoorholderApplicationTests.java
@@ -1,20 +1,20 @@
 package com.aps.shop;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 @SpringBootTest
 class DoorholderApplicationTests {
 
-	@Autowired
-	private ApplicationContext applicationContext;
+    @Autowired
+    private ApplicationContext applicationContext;
 
-	@Test
-	void contextLoads() {
-		assertNotNull(applicationContext);
-	}
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext);
+    }
 }

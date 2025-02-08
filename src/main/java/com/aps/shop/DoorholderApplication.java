073bababa475bdcfc1bec1befd85ffbcaaa5d24d
Austin Schaefer <schaefer.austin.p@inbox.eu>
2025-02-08 19:53:26 +0100

    Create project skeleton.

---
+++ src/main/java/com/aps/shop/DoorholderApplication.java
@@ -0,0 +1,13 @@
+package com.aps.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoorholderApplication.class, args);
+	}
+
+}
